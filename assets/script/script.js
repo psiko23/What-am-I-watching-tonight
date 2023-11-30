b04cff166a94d8348665bbeba382c910b964f432
Psiko23 <tylerpittman2001@gmail.com>
2023-11-29 18:56:56 -0800

    added fetch

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,10 +1,12 @@
-fetch('https://imdb188.p.rapidapi.com/api/v1/searchIMDB?query=%3CREQUIRED%3E')
-.then(function (response) {
-    response.json();
-    console.log(response);
-    return response;
-})
-.then(function (data) {
-    console.log(data);
-    return data;
-})
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMmIxNjJjYjdhNzJiMTQ3YzMwZWJkNGQ1ZTMwZjk5NCIsInN1YiI6IjY1NjU1OWJlYThiMmNhMDBjOTg2MDMyOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZTs5QJnobmaJOzvPBH6vTdoLBBmBF9gcsN_8fyRzYWk'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/configuration', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
